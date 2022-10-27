fab0ae3f3c3448d8106b85ce7aefa9114b482b13
lukjjz95 <lukjjz.dev@gmail.com>
2022-10-26 23:21:03 -0300

    update

--- main.js
+++ main.js
@@ -1,71 +1,58 @@
-
-let nombre = "";
-let ingreso = "";
-let salida = "";
-let adultos = "";
-let ninios = "";
-pedirDatos();
-comenzarReservacion();
-
-
-function pedirDatos() { //pido datos al cliente para comenzar su reserva
-    do {
-        nombre = prompt("ingrese su nombre");
-        ingreso = prompt("Ingrese su fecha de ingreso");
-        salida = prompt("ingrese su fecha de salida");
-        adultos = prompt("cuantos adultos?");
-        ninios = prompt("cuantos niños?");
-    } while (!datosValidos(nombre, ingreso, salida, adultos, ninios));
-
-
-
-
-
+// Class
+class Paquete {
+    constructor(nombre, precio, id) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.id = id;
+    }
 }
 
-function datosValidos(nombre, ingreso, salida, adultos, ninios) {
-    if ((nombre !== "" && ingreso !== "" && salida !== "" && adultos !== "" && ninios !== "")
+let lista_compra = [];
 
-        &&
+//  array de paquetes vacío
+const paquetes = [];
+paquetes.push(new Paquete("- Transporte redondo y entrada a Six Flags ($450)", 450, 1));
+paquetes.push(new Paquete("- Chevallier . ($200)", 200, 2));
+paquetes.push(new Paquete("- Flechabus Premium. ($300)", 300, 3));
+paquetes.push(new Paquete("- Transporte Clasico el jujeño (ida o vuelta).  ($120)", 120, 4));
 
-        (nombre !== null && ingreso !== null && salida !== null && adultos !== null && ninios !== null)) {
-        return true;
+//  mostrar la lista
+const mostrarLista = () => {
+    let texto = "";
+    paquetes.forEach((item) => {
+        texto += `${item.id}) ${item.nombre}\n`;
+    });
+
+    //  Capturo las entradas 
+    let seleccion = parseInt(prompt(`Selecciona el paquete deseado.\n${texto}`));
+    return seleccion;
+};
+
+//  Función búsqueda del paquete y agregarlo a mi lista de compra.
+const paqueteElegido = (id) => {
+    console.log(id);
+    let paqueteFind = paquetes.find((item) => item.id === id);
+    lista_compra.push(paqueteFind);
+    let continuar = confirm("¿Desea agregar otro paquete? \n(Aceptar = Si) (Cancelar = No)");
+    if (continuar === true) {
+        iniciaVenta();
     }
-    else {
-        alert("revise los datos ingresados, por favor e intente nuevamente PERRITO");
-    }
+};
+
+//  inicio la venta.
+const iniciaVenta = () => {
+    paqueteId = mostrarLista();
+    paqueteElegido(paqueteId);
+};
 
 
+iniciaVenta();
 
 
-
-}
-
-function comenzarReservacion() {
-    alert("Su reserva fue realizada con exito " + nombre + "\n"
-        + "Reserva con fecha de ingreso para el: " + ingreso + "\n"
-        + "Con fecha de salida para el: " + salida + "\n"
-        + "Cantidad adultos: " + adultos + "\n"
-        + "Cantidad de niños: " + ninios
-    );
-
-    let resp = confirm("Desea concretar la compra de sus pasajes?:");
-
-    if (resp) {
-
-        alert("Gracias por reservar en hoteljs, la pagina se redireccionara al sector de pagos.");
-
-    }
-
-    else {
-
-        alert("Aguardamos su futura compra" + "\n" +
-            "No dude en contacarse con nosotros via redes sociales o al 0800-521-4567, gracias.");
-    }
+let total = lista_compra.reduce((acc, item) => acc + item.precio, 0);
+//  contar paquetes array
+let numeroPaquetes = lista_compra.length;
 
 
-
-
-
-}
-
+alert("Resúmen de compra:\n\n" + "Total de paquetes: " + numeroPaquetes + "\n Total a pagar: " + total);
+alert("Gracias por su compra, ¡Buen Viaje");
